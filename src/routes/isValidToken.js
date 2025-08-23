51bf524bac321bf1c0e6e5f8abe73e107c7f25d5
= <animesh7995@gmail.com>
2025-08-23 20:15:39 +0530

    fix is valid token for admin

--- src/routes/isValidToken.js
+++ src/routes/isValidToken.js
@@ -7,4 +7,8 @@ router.post('/', util.verifyStoreName, token.verifyAuthToken, (req, res, next) =
     console.log(`User login check ${req.customer_id}`);
     res.status(200).json({is_valid_user: true});
 });
+router.post('/admin', util.verifyStoreName, token.verifyAdminAuthToken, (req, res, next) => {
+    console.log(`Admin User login check ${req.admin_user_id}`);
+    res.status(200).json({is_valid_admin: true});
+});
 module.exports = router;
